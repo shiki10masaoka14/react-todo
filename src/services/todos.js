b2783548da448f9e64fdaeaa783921e2903605d1
shiki10masaoka14 <shiki10masaoka14@gmail.com>
2021-01-03 12:23:50 +0900

    getAll

--- src/services/todos.js
+++ src/services/todos.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const baseUrl = "http://localhost:3001/todos";
 
-export const getAll = async () => {
+const getAll = async () => {
   const response = await axios.get(baseUrl);
   return response.data;
 };
 
-// default {getAll};
+export default getAll;
